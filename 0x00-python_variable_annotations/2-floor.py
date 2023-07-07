fde9315d4dbec4594b04edac347fa46d82ff4ae8
Cyberguru1 <hamzacypher34@gmail.com>
2023-07-06 21:44:58 -0500

    new test case

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+function that calculates the floor of a number.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of a float."""
+    return math.floor(n)
