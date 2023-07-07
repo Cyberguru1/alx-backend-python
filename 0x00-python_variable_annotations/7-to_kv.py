c5c5ef52f9f8501a4cea176f9e59743525a85776
Cyberguru1 <hamzacypher34@gmail.com>
2023-07-06 21:49:47 -0500

    new test case

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Function that converts a Python variable to a KV pair.
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Converts a Python variable to a KV pair."""
+    return k, v ** 2
