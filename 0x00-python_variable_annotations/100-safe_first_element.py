537c9cd61cfdd2b4168008c93e39dbf1cf8b95ad
Cyberguru1 <hamzacypher34@gmail.com>
2023-07-06 21:53:18 -0500

    new test case

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Augmented code with the correct duck-typed annotations.
+"""
+from typing import Any, Union, Sequence
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Return the first element of a list or None if the list is empty."""
+    if lst:
+        return lst[0]
+    else:
+        return None
