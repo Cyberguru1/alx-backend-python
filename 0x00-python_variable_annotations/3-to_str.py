08f3d75813d85f3ce6430d1ed3675dfa4f35e74e
Cyberguru1 <hamzacypher34@gmail.com>
2023-07-06 21:45:49 -0500

    new test case

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+function that returns a string reps of a float
+"""
+
+
+def to_str(n: float) -> str:
+    """Returns a string reps of a float
+    Args:
+        n: A float to convert to a string
+    Returns:
+        A string reps of the float
+    """
+    return str(n)
