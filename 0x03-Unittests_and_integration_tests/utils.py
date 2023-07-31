836c5c25ead92483274ef78ec6f89a209ae8bbf0
Cyberguru1 <hamzacypher34@gmail.com>
2023-07-31 18:46:49 -0500

    task: fixtures

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -1,22 +1,3 @@
-
-
-Settings
-
-Hi! Here some our recommendations to get the best out of BLACKBOX:
-
-Be as clear as possible
-
-End the question in what language you want the answer to be, e.g: ‘connect to mongodb in python
-or you can just
-Watch tutorial video
-Here are some suggestion (choose one):
-Write a function that reads data from a json file
-How to delete docs from mongodb in phyton
-Connect to mongodb in nodejs
-Ask any coding question
-send
-refresh
-Blackbox AI Chat is in beta and Blackbox is not liable for the content generated. By using Blackbox, you acknowledge that you agree to agree to Blackbox's Terms and Privacy Policy
 #!/usr/bin/env python3
 """Generic utilities for github org client.
 """
